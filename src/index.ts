5b220aec9555b5ebe37fafebd29eb057163c8ce5
cuongne <cuontruon007@gmail.com>
2025-05-13 00:10:08 +0700

    c

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { Checkbox } from './components/Checkbox';
-
-export function calc(a: number, b: number) {
-    return a + b;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,27 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-import react from "@vitejs/plugin-react";
+
 import { defineConfig } from "vite";
-import path from "path";
+import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-
-
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(),dts()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Alias `@` to `src`
-    },
-  },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'TaskManagement',
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "oke",
       fileName: (format) => `index.${format}.js`,
-
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
-    outDir: 'dist',
     sourcemap: true,
+    emptyOutDir: true,
   },
+  plugins: [react(), dts()],
 });
